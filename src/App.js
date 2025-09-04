f390e4488941a677f883f3fd8bdcb65b243c03c7
MILITA2@365.openu.ac.il <MILITA2@365.openu.ac.il>
2025-09-04 14:41:24 +0300

    login page looks good

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import LoginPage from './components/LoginPage';
+import {createGlobalStyle} from 'styled-components';
+
+
+const GlobalStyle = createGlobalStyle`
+    :root {
+        --army-green: #3b5d3a;
+        --army-green-dark: #2f4a2e;
+        --olive: #6b8f5b;
+        --khaki: #a89f7b;
+        --sand: #d2c29d;
+        --black-80: rgba(0, 0, 0, 0.8);
+        --black-60: rgba(0, 0, 0, 0.6);
+        --accent: #c7d36f;
+        --accent-2: #e8f2a9;
+        --danger: #ff6b6b;
+    }
+`;
+
 
 function App() {
-  return (
-    <LoginPage />
-  );
+    return (<>
+            <GlobalStyle/>
+            <LoginPage/>
+        </>
+    )
 }
 
 export default App;
